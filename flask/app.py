1353f0984e9f8d0fd950a152a1efcb6ef097f992
root <root@DESKTOP-RHL201E.localdomain>
2026-06-16 15:37:44 +0400

    nginx reverse proxy with flask backend

---
+++ flask/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "yo this is coming from Flask through nginx"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
